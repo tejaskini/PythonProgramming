9a7d37cc1cf732cad06efd2d456477dbccc206b3
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-11-21 11:54:43 +0530

    Introduction to python

---
+++ py_1_intro.py
@@ -0,0 +1,23 @@
+'''
+Hi, This is Tejas, Here is the step by step codes and programs
+you understand better and for leaning the python in easy way subscribe my
+Youtube Channel !!
+https://www.youtube.com/@CodeChess-14t
+https://in.linkedin.com/in/tejas-kini-86172421b
+
+'''
+
+
+print("hello learners, Welcome Back!!!")
+
+note = """
+    -Python is High level programming language and most popular!!
+    -Used on server side
+    -Interpreted language
+    -Easy to lean and understand , simple syntax
+    -Platform Idependent
+"""
+
+print(note)
+
+#Just write print and you will see magic !!!
