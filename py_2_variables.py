d1102410b8c3f8ee18d3db78861dc07ddc6c9019
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-11-22 14:25:17 +0530

    Variables in python

---
+++ py_2_variables.py
@@ -0,0 +1,23 @@
+"""
+Variable in python does not have any command to specify weather it is int string or float
+Just write var name and assign the value
+Variables : it is used to store value 
+
+Variable name should not start with number or any special character
+should staart with (_Underscore) and Letter
+should not any python keyword(eg, print, if, for etc..)
+
+"""
+
+firstNum = 44
+print(firstNum)
+Name = "CodeChess"
+print(Name)
+FloatVal = 66.6
+print(FloatVal)
+
+""" You can follow a namig Convension
+1.camelCase - yourNameIs = "CodeChess"
+2.PascalCase- YourNameIs = "CodeChess"
+3.Snake -     Your_Name_Is = "CodeChess"
+"""
